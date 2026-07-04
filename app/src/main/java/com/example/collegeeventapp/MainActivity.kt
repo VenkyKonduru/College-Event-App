c1ceadf3095def0af19f125ccf8726e1aa6454ab
VenkyKonduru <venkykonduru66@gmail.com>
2026-07-04 14:12:12 +0530

    Added login validation using Toast and input checks

--- app/src/main/java/com/example/collegeeventapp/MainActivity.kt
+++ app/src/main/java/com/example/collegeeventapp/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.collegeeventapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +15,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        val etEmail = findViewById<EditText>(R.id.etEmail)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+
+        btnLogin.setOnClickListener {
+
+
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+
+            if(email.isBlank()) {
+                Toast.makeText(this,"Enter Email", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if(password.isBlank()) {
+                Toast.makeText(this,"Enter Password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+
+            }
+
+            val intent = Intent(this, RegisterActivity::class.java)
+
+            startActivity(intent)
+        }
+
+        val btnRegister = findViewById<Button>(R.id.btnRegister)
+        btnRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
