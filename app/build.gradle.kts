ca1d090bdc7e4d08bb486812f4150c94bc67240f
VenkyKonduru <venkykonduru66@gmail.com>
2026-07-06 11:34:13 +0530

    Added Firebase Authentication for user registration and Saved user details to Firestore after registration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
-
 android {
     namespace = "com.example.collegeeventapp"
     compileSdk {
@@ -34,11 +34,23 @@ android {
 }
 
 dependencies {
+
+    // Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:34.15.0"))
+
+    // Firebase Authentication
+    implementation("com.google.firebase:firebase-auth")
+
+
+    implementation("com.google.firebase:firebase-firestore")
+
+
     implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/example/collegeeventapp/RegisterActivity.kt
+++ app/src/main/java/com/example/collegeeventapp/RegisterActivity.kt
@@ -1,20 +1,124 @@
 package com.example.collegeeventapp
 
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import android.content.Intent
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.jar.Attributes
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        auth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
+
+        val rgRole = findViewById<RadioGroup>(R.id.rgRole)
+        val etName = findViewById<EditText>(R.id.etName)
+        val etEmail = findViewById<EditText>(R.id.etEmail)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val etConfirmPassword = findViewById<EditText>(R.id.etConfirmPassword)
+
+
+        val btnRegister = findViewById<Button>(R.id.btnRegister)
+
+        btnRegister.setOnClickListener {
+            val email = etEmail.text.toString()
+            val name = etName.text.toString()
+            val password = etPassword.text.toString()
+            val confirmPassword = etConfirmPassword.text.toString()
+            val selectedRole = rgRole.checkedRadioButtonId
+
+
+            if (email.isBlank()) {
+                Toast.makeText(this, "Please enter Email", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (name.isBlank()) {
+                Toast.makeText(this, "Please enter Name", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (password.isBlank()) {
+                Toast.makeText(this, "Please enter Password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (confirmPassword.isBlank()) {
+                Toast.makeText(this, "Please Confirm your Password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (password != confirmPassword) {
+                Toast.makeText(this, "Password Does not match", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (selectedRole == -1) {
+                Toast.makeText(this, "Please select a Role", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val role =
+                if (selectedRole == R.id.rbAdmin)
+                    "Admin"
+                else
+                    "Student"
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+
+                        val uid = auth.currentUser!!.uid
+                        val user = hashMapOf(
+                            "name" to name,
+                            "email" to email,
+                            "role" to role
+                        )
+                        db.collection("users")
+                            .document(uid)
+                            .set(user)
+                            .addOnSuccessListener {
+                                Toast.makeText(this, "Registration Successful", Toast.LENGTH_SHORT)
+                                    .show()
+
+                                val intent = Intent(this, MainActivity::class.java)
+                                startActivity(intent)
+                            }
+                            .addOnFailureListener { e ->
+                                Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+                            }
+
+                    } else {
+                        Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+
         }
+
+        val btnLogin= findViewById<Button>(R.id.btnLogin)
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
+
     }
 }
